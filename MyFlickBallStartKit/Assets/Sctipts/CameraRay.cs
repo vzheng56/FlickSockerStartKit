e4e0bdf07aa127ab0091470e5fb974d8ecb214c2
渔夫 <vzheng56@gmail.com>
2015-04-14 22:24:01 +0800

    add some function
    添加渲染效果功能；
    相机
    渲染到材质
    UI与相机的关系等

---
+++ MyFlickBallStartKit/Assets/Sctipts/CameraRay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRay : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit cameraHit;
+
+		if (Physics.Raycast (camRay, out cameraHit,1000)) {
+
+			Debug.Log(cameraHit.collider.gameObject.name);
+		}
+	
+	}
+}
---
+++ MyFlickBallStartKit/Assets/Sctipts/UISocker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISocker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate (Vector3.up,100*Time.deltaTime);
+	}
+}
